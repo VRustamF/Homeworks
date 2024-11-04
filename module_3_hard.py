e73b7f1c61234f06ad49a7855b5c4f291fedb065
VRustamF <118049337+VRustamF@users.noreply.github.com>
2024-11-04 17:34:35 +0700

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,43 @@
+def calculate_structure_sum(data_structure):
+    global count
+
+    if isinstance(data_structure, str):
+        count += len(data_structure)
+        return count
+
+    elif isinstance(data_structure, int):
+        count += data_structure
+        return count
+
+    elif isinstance(data_structure, list):
+        for el in data_structure:
+            calculate_structure_sum(el)
+
+    elif isinstance(data_structure, dict):
+        for el in data_structure.items():
+            calculate_structure_sum(el)
+
+    elif isinstance(data_structure, tuple):
+        for el in data_structure:
+            calculate_structure_sum(el)
+
+    elif isinstance(data_structure, set):
+        for el in data_structure:
+            calculate_structure_sum(el)
+
+    return count
+
+
+count = 0
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+
+print(result)
